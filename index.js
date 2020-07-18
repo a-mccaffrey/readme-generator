031cb13eee72714a83e72b4ae16bb810416df04b
a-mccaffrey <aisling.s.mccaffrey@outlook.com>
2020-07-18 19:30:05 -0400

    testing github url in readme

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ ${tests}
 
 ## Questions
 
-Want to know more? Look me up on GitHub. My username is ${github} and my profile is at [github.com/${github}](github.com/${github})
+Want to know more? Look me up on GitHub. My username is ${github} and my profile is [here](github.com/${github})
 
 Alternatively, get in touch by emailing me at [${email}](mailto:${email}).
 `;
